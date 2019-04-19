192ad74764112618e67056f056ee61d0fa2e4d70
Joschi Kuphal <joschi@tollwerk.de>
2019-04-19 13:07:46 +0200

    Add icon use reference viewhelper

---
+++ Classes/ViewHelpers/Icon/IconViewHelperTrait.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * tollwerk
+ *
+ * @category   Tollwerk
+ * @package    Tollwerk\Vamoso
+ * @subpackage Tollwerk\TwBase\ViewHelpers\Icon
+ * @author     Joschi Kuphal <joschi@tollwerk.de> / @jkphl
+ * @copyright  Copyright © 2019 Joschi Kuphal <joschi@tollwerk.de> / @jkphl
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+/***********************************************************************************
+ *  The MIT License (MIT)
+ *
+ *  Copyright © 2019 Joschi Kuphal <joschi@tollwerk.de>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *  this software and associated documentation files (the "Software"), to deal in
+ *  the Software without restriction, including without limitation the rights to
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ *  the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ *  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ *  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ *  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ***********************************************************************************/
+
+namespace Tollwerk\TwBase\ViewHelpers\Icon;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+use TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+
+/**
+ * Shared methods for icon viewhelpers
+ *
+ * @package    Tollwerk\TwBase
+ * @subpackage Tollwerk\TwBase\ViewHelpers
+ */
+trait IconViewHelperTrait
+{
+    /**
+     * Icon root paths
+     *
+     * @var string[]|null
+     */
+    protected static $iconRootPaths = null;
+
+    /**
+     * Find an icon and return the absolute icon path
+     *
+     * @param string $icon Icon name
+     *
+     * @return string Icon file path
+     * @throws InvalidConfigurationTypeException
+     * @throws \OutOfBoundsException If the icon is unknown
+     */
+    protected function getIconFile(string $icon): string
+    {
+        // Search for the icon in the given icon root path order
+        foreach ($this->getIconRootPaths() as $iconRootPath) {
+            $iconFile = GeneralUtility::getFileAbsFileName($iconRootPath.$icon);
+            if (is_file($iconFile)) {
+                return $iconFile;
+            }
+        }
+
+        throw new \OutOfBoundsException($icon, 1549185715);
+    }
+
+    /**
+     * Return the list of icon root paths
+     *
+     * @return string[] Icon root paths
+     * @throws InvalidConfigurationTypeException
+     */
+    protected function getIconRootPaths(): array
+    {
+        if (self::$iconRootPaths === null) {
+            self::$iconRootPaths  = [];
+            $objectManager        = GeneralUtility::makeInstance(ObjectManager::class);
+            $configurationManager = $objectManager->get(ConfigurationManager::class);
+            $settings             = $configurationManager->getConfiguration(
+                ConfigurationManager::CONFIGURATION_TYPE_SETTINGS,
+                'TwBase'
+            );
+            self::$iconRootPaths  = array_map(
+                function($rootPath) {
+                    return rtrim($rootPath, '/').'/';
+                },
+                GeneralUtility::trimExplode(',', $settings['icons']['iconRootPath'], true)
+            );
+        }
+
+        return self::$iconRootPaths;
+    }
+}
--- Classes/ViewHelpers/Icon/UserefViewHelper.php
+++ Classes/ViewHelpers/Icon/UserefViewHelper.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
- * data
+ * tollwerk
  *
  * @category   Tollwerk
- * @package    Tollwerk\TwBase
- * @subpackage Tollwerk\TwBase\ViewHelpers
+ * @package    Tollwerk\Vamoso
+ * @subpackage Tollwerk\TwBase\ViewHelpers\Icon
  * @author     Joschi Kuphal <joschi@tollwerk.de> / @jkphl
- * @copyright  Copyright © 2018 Joschi Kuphal <joschi@tollwerk.de> / @jkphl
+ * @copyright  Copyright © 2019 Joschi Kuphal <joschi@tollwerk.de> / @jkphl
  * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
  */
 
 /***********************************************************************************
  *  The MIT License (MIT)
  *
- *  Copyright © 2019 tollwerk GmbH <info@tollwerk.de>
+ *  Copyright © 2019 Joschi Kuphal <joschi@tollwerk.de>
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy of
  *  this software and associated documentation files (the "Software"), to deal in
@@ -37,25 +37,43 @@
 namespace Tollwerk\TwBase\ViewHelpers\Icon;
 
 use Tollwerk\TwBase\Utility\SvgIconManager;
-use Tollwerk\TwBase\ViewHelpers\IconViewHelper;
+use TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
-/**
- * <use> Icon ViewHelper for
- *
- * @package    Tollwerk\TwBase
- * @subpackage Tollwerk\TwBase\ViewHelpers
- */
-class UseViewHelper extends IconViewHelper
+class UserefViewHelper extends AbstractViewHelper
 {
     /**
-     * Get the icon DOM
-     *
-     * @param string $iconFile Icon file path
-     *
-     * @return \DOMDocument Icon DOM
+     * Use shared icon viewhelper methods
      */
-    protected function getIconDom(string $iconFile): \DOMDocument
+    use IconViewHelperTrait;
+
+    /**
+     * Initialize arguments
+     *
+     * @api
+     */
+    public function initializeArguments(): void
     {
-        return SvgIconManager::useIcon($iconFile);
+        parent::initializeArguments();
+        $this->registerArgument('icon', 'string', 'Name of the icon', true);
     }
+
+    /**
+     * Render the icon
+     *
+     * @return string Rendered icon
+     * @throws InvalidConfigurationTypeException
+     * @api
+     */
+    public function render(): string
+    {
+        try {
+            $icon = ucfirst(pathinfo($this->arguments['icon'], PATHINFO_FILENAME)).'.svg';
+
+            return SvgIconManager::useIconReference($this->getIconFile($icon));
+        } catch (\OutOfBoundsException $e) {
+            return '<!-- Unknown SVG icon "'.$e->getMessage().'" -->';
+        }
+    }
+
 }
