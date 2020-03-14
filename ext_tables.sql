e2598b50b28a82f5b9c60d72bbbf2c7d59bb142b
Joschi Kuphal <joschi@tollwerk.de>
2020-03-14 16:50:07 +0100

    Add file author & license options

--- ext_tables.sql
+++ ext_tables.sql
@@ -25,25 +25,39 @@ CREATE TABLE tt_content
 );
 
 #
+# Modifying sys_file_metadata table
+#
+CREATE TABLE sys_file_metadata
+(
+    tx_twbase_author        TINYTEXT    DEFAULT '' NOT NULL,
+    tx_twbase_author_url    TINYTEXT    DEFAULT '' NOT NULL,
+    tx_twbase_source_url    TINYTEXT    DEFAULT '' NOT NULL,
+    tx_twbase_creation_year int(11) unsigned,
+    tx_twbase_license       VARCHAR(12) DEFAULT '' NOT NULL,
+    tx_twbase_license_name  TINYTEXT    DEFAULT '' NOT NULL,
+    tx_twbase_license_url   TINYTEXT    DEFAULT '' NOT NULL,
+);
+
+#
 # Table structure for table 'tx_twbase_domain_model_video_track'
 #
 CREATE TABLE tx_twbase_domain_model_video_track
 (
-    uid       int(11)                          NOT NULL auto_increment,
-    pid       int(11)              DEFAULT '0' NOT NULL,
+    uid        int(11)                                 NOT NULL auto_increment,
+    pid        int(11)              DEFAULT '0'        NOT NULL,
 
-    kind      varchar(12)          DEFAULT 'captions'  NOT NULL,
-    file      int(11) unsigned     DEFAULT '0' NOT NULL,
-    language  VARCHAR(16)          DEFAULT ''  NOT NULL,
-    transcript  TEXT,
+    kind       varchar(12)          DEFAULT 'captions' NOT NULL,
+    file       int(11) unsigned     DEFAULT '0'        NOT NULL,
+    language   VARCHAR(16)          DEFAULT ''         NOT NULL,
+    transcript TEXT,
 
-    tstamp    int(11) unsigned     DEFAULT '0' NOT NULL,
-    crdate    int(11) unsigned     DEFAULT '0' NOT NULL,
-    cruser_id int(11) unsigned     DEFAULT '0' NOT NULL,
-    deleted   smallint(5) unsigned DEFAULT '0' NOT NULL,
-    hidden    smallint(5) unsigned DEFAULT '0' NOT NULL,
-    starttime int(11) unsigned     DEFAULT '0' NOT NULL,
-    endtime   int(11) unsigned     DEFAULT '0' NOT NULL,
+    tstamp     int(11) unsigned     DEFAULT '0'        NOT NULL,
+    crdate     int(11) unsigned     DEFAULT '0'        NOT NULL,
+    cruser_id  int(11) unsigned     DEFAULT '0'        NOT NULL,
+    deleted    smallint(5) unsigned DEFAULT '0'        NOT NULL,
+    hidden     smallint(5) unsigned DEFAULT '0'        NOT NULL,
+    starttime  int(11) unsigned     DEFAULT '0'        NOT NULL,
+    endtime    int(11) unsigned     DEFAULT '0'        NOT NULL,
 
     PRIMARY KEY (uid),
     KEY parent (pid),
